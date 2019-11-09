da2439e3369f70b11122814ce30c64443a9cca8e
Gadzhik <gkurban@outlook.com>
2019-11-09 19:59:19 +0300

    Update disk_spaced.sh

--- disk_spaced.sh
+++ disk_spaced.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 use_disk=`df -h / | grep -v Filesystem | awk '{print $5}' | sed 's/%//g'`
 case $use_disk in
-[1-84]*)
-echo "OK - $use_disk% of disk space used."
+[1-89]*)
+echo "OK - $use_disk% available disk space."
 exit 0
 ;;
 [90]*)
-echo "WARNING - $use_disk% of disk space used."
+echo "WARNING - $use_disk% available disk space."
 exit 1
 ;;
 [91-100]*)
-echo "CRITICAL - $use_disk% of disk space used."
+echo "CRITICAL - $use_disk% available disk space."
 exit 2
 ;;
 *)
-echo "UNKNOWN - $use_disk% of disk space used."
+echo "UNKNOWN - $use_disk% available disk space."
 exit 3
 ;;
 esac
